0ac6ad2fdfc6090d6961e0aeb71afb0b7f80eb2c
Isaac Emmanuel Muñoz Flores <70903937+Isaacmunoz815@users.noreply.github.com>
2020-10-05 20:49:10 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,15 +6,11 @@ int main() {
 	int edad;
 	char nombre[30];
 	
-	cout<<"Escribe tu nombre: \n";
 	cin.getline(nombre, 30);
-	cout<<"Escribe tu edad: \n";
 	cin>>edad;
 	
-	cout<<<"______________BYTES_________________";
-	
-	cout<<"la cantidad de bytes (nombre) es de: "<<sizeof(nombre)<<endl;
-	cout<<"la cantidad de bytes (edad) es de: "<<sizeof(edad)<<endl;
+	cout<<sizeof(nombre)<<endl;
+	cout<<sizeof(edad)<<endl;
 
     return 0;
 }
